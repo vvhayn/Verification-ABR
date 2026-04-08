5591ccfdac5defcbd364ad7a76d9c6e152c6bba4
Raphael <liberaleraphael@gmail.com>
2026-04-08 12:12:53 +0200

    --allow-empty-message

--- est_ABR_V2.c
+++ est_ABR_V2.c
@@ -72,27 +72,27 @@ void affiche_infos(Arbre a) {
     printf("Le nombre de visites pour déterminer si c'est un ABR est : %lld\n", tmp);
 }
 
-int main(void){
-    /* Arbre a : non-ABR */
-    Arbre a = alloue_noeud(1);
-    a->fg         = alloue_noeud(2);
-    a->fg->fd     = alloue_noeud(42);
+// int main(void){
+//     /* Arbre a : non-ABR */
+//     Arbre a = alloue_noeud(1);
+//     a->fg         = alloue_noeud(2);
+//     a->fg->fd     = alloue_noeud(42);
 
-    /* Arbre b : ABR */
-    Arbre b = alloue_noeud(10);
-    b->fg         = alloue_noeud(5);
-    b->fg->fg     = alloue_noeud(2);
-    b->fg->fd     = alloue_noeud(7);
-    b->fd         = alloue_noeud(20);
-    b->fd->fg     = alloue_noeud(15);
-    b->fd->fd     = alloue_noeud(30);
+//     /* Arbre b : ABR */
+//     Arbre b = alloue_noeud(10);
+//     b->fg         = alloue_noeud(5);
+//     b->fg->fg     = alloue_noeud(2);
+//     b->fg->fd     = alloue_noeud(7);
+//     b->fd         = alloue_noeud(20);
+//     b->fd->fg     = alloue_noeud(15);
+//     b->fd->fd     = alloue_noeud(30);
 
-    printf("Arbre a :\n");
-    affiche_infos(a);
+//     printf("Arbre a :\n");
+//     affiche_infos(a);
 
-    printf("\nArbre b :\n");
-    affiche_infos(b);
+//     printf("\nArbre b :\n");
+//     affiche_infos(b);
 
-    return 0;
-}
+//     return 0;
+// }
 
